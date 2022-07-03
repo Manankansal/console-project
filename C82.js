b6ab52d6f081e0c8e8fdee435cb47de5af18b068
Manankansal <108222380+Manankansal@users.noreply.github.com>
2022-07-03 16:25:55 +0530

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+            var first_name = "Ranbir";
+            var last_name = "Kapoor";
+
+            var full_name = first_name.concat(last_name);
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+            var add = 10 + 28;
+            console.log(add);
+
+            var sub = 38 - 12;
+            console.log(sub);
+
+            var multiple =2 * 44;
+            console.log(multiple);
+
+            var divide = 15 / 3 ;
+            console.log(divide);
